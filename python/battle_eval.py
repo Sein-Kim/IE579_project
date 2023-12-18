40b202f51cf8b1c821186bf2ca229dbb6c0bd6ac
Sein-Kim <rlatpdlsgns81@gmail.com>
2023-12-18 15:56:29 +0900

    residual

--- python/battle_eval.py
+++ python/battle_eval.py
@@ -1,7 +1,7 @@
 import time
 
 import numpy as np
-
+import torch
 from battle_env import MAgentBattle
 from agent.agent_rl.agent_rl import AgentRL
 from agent.agent_rule.agent_random import AgentRandom
@@ -14,7 +14,12 @@ if __name__ == "__main__":
 
     env = MAgentBattle(visualize=False, eval_mode=True)
 
-    agent1 = [AgentRandom(num_agent=1, dim_obs=env.dim_obs, dim_action=env.dim_action) for _ in range(env.num_agent)]
+    agent1 = [AgentRL(num_agent=1, dim_obs=env.dim_obs, dim_action=env.dim_action) for _ in range(env.num_agent)]
+    
+    for _ in agent1:
+        load_dict = torch.load('./agent_rl.pt')
+        _.load_state_dict(load_dict)
+    
     agent2 = [AgentRandom(num_agent=1, dim_obs=env.dim_obs, dim_action=env.dim_action) for _ in range(env.num_agent)]
 
     (obs1, obs2), (done1, done2, done_env), (valid1, valid2) = env.reset()
